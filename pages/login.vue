5e8b1a93217696f11e90f0e3aa03c36d444ecf95
kiwicopple <pcopplestone@gmail.com>
2018-08-31 18:04:14 +0800

    login formatting

--- pages/login.vue
+++ pages/login.vue
@@ -1,45 +1,49 @@
 <template>
 <div>
-  <div v-if="error">{{ error + '' }}</div>
-  <div v-if="$auth.$state.redirect">
-    You have to login before accessing to <strong>{{ $auth.$state.redirect }}</strong>
-  </div>
   <div class="section columns is-centered">
 
       <div class="column is-8">
+
+        <div v-if="$auth.$state.redirect" class="notification is-danger">
+          You have to login before accessing to <strong>{{ $auth.$state.redirect }}</strong>
+        </div>
+
+        <div v-if="error" class="notification is-warning">
+          {{ error + '' }}
+        </div>
+
         <h4 class="title is-4">Login</h4>
+
         <div class="box">
           <div class="field">
             <p class="control has-icons-left">
               <input class="input" type="email" placeholder="Username" v-model="username">
-              <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
+              <span class="icon is-left"><i class="fas fa-envelope"></i></span>
             </p>
           </div>
           <div class="field">
             <p class="control has-icons-left">
               <input class="input" type="password" placeholder="Password" v-model="password">
-              <span class="icon is-small is-left"><i class="fas fa-lock"></i></span>
+              <span class="icon is-left"><i class="fas fa-lock"></i></span>
             </p>
           </div>
           <div class="field">
-            <p class="control">
-              <button class="button is-fullwidth is-primary"  @click="login">
-                LOGIN
-              </button>
-            </p>
+            <div class="buttons is-right">
+              <button class="button is-rounded is-primary" @click="login">LOGIN</button>
+            </div>
           </div>
         </div>
-        
+
         <div class="box" v-if="strategies.length">
-          <div v-for="s in strategies" :key="s.key" class="mb-2">
-          <a @click="$auth.loginWith(s.key)" class="button is-fullwidth is-dark">Login with {{ s.name }}</a>
+          <div v-for="s in strategies" :key="s.key" class="">
+            <a @click="$auth.loginWith(s.key)" class="button is-fullwidth is-rounded is-outlined is-dark">Login with {{ s.name }}</a>
           </div>
         </div>
 
       </div>
 
-        
-        
+
+
   </div>
 </div>
 </template>
